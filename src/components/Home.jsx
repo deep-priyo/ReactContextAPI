eba0fc6631c67c54c2bd72c317ec918f3a0435f0
Priyadeep Mullick <129691885+deep-priyo@users.noreply.github.com>
2024-11-28 22:17:28 +0530

    Updated Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
-import User from "./User.jsx";
+import React from 'react';
 
 const Home = () => {
     return (
-        <div>
-            <h1>home</h1>
-            <User/>
+        <div className="flex flex-col items-center justify-center h-full text-center p-5">
+            <h1 className="text-4xl font-bold mb-4">Welcome to My App</h1>
+            <p className="text-lg text-gray-600">
+                This is a simple app that demonstrates using React Context to share data across components without the need for prop drilling. Check out User for more...
+            </p>
         </div>
-    )
-}
-export default Home
+    );
+};
+
+export default Home;
