0d60f65ebd8292c9f4b6573f0319c40e45aec618
Priyadeep Mullick <129691885+deep-priyo@users.noreply.github.com>
2024-11-28 20:22:07 +0530

    Configured routing

---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import User from "./User.jsx";
+
+const Home = () => {
+    return (
+        <div>
+            <h1>home</h1>
+            <User/>
+        </div>
+    )
+}
+export default Home
---
+++ src/components/User.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import UserDetail from "./UserDetail.jsx";
+
+const User = () => {
+    return (
+        <div>
+            <h1>user</h1>
+            <UserDetail/>
+        </div>
+    )
+}
+export default User
